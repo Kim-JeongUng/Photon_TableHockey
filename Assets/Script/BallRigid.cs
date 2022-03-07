d2fd95aa589b579bac62167ddb97dd6e57ba8608
Kim JeongUng <82865325+Kim-JeongUng@users.noreply.github.com>
2022-03-07 18:39:58 +0900

    반사각 OK
    - 점수 동기화

--- Assets/Script/BallRigid.cs
+++ Assets/Script/BallRigid.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class BallRigid : MonoBehaviour
 {
-    GameObject[] Bat;
-    float batRadius = 3;
-    float ballRadius = 4;
-
-    float distance;
-    float collisionDistance;
-    Vector3 prevPos;
-
     Vector3 ballDir = Vector3.zero;
     float ballSpeed = 0.1f;
+
     Vector3 pos;
+
     // Start is called before the first frame update
     void Start()
     {
-        Bat = GameObject.FindGameObjectsWithTag("Bat");
-        collisionDistance = batRadius / 2 + ballRadius / 2;
-        prevPos = Vector3.zero;
         pos = transform.position;
     }
 
@@ -31,7 +22,6 @@ public class BallRigid : MonoBehaviour
 
         Vector3 Pos = transform.position;
         Collider[] cols = Physics.OverlapSphere(transform.position, 2.0f); // 반경 내의 모든 Collider 탐색
-        transform.position = new Vector3(transform.position.x, 1.0f, transform.position.z);
 
         if (cols.Length >1)
         {
@@ -41,20 +31,24 @@ public class BallRigid : MonoBehaviour
             //충돌
             for (int i = 0; i < cols.Length; i++)
             {
-                distance = Vector3.Distance(this.transform.position, cols[i].transform.position);
                 if (!cols[i].CompareTag("Ball"))
                 {
-                    if (cols[i].CompareTag("Wall"))//Pos != prevPos)
-                        ballDir = Vector3.Reflect(ballDir.normalized, Vector3.zero); //cols[i].transform.position - transform.position));//new Vector3(cols[i].transform.position.x, 1, cols[i].transform.position.z));
-                    if (cols[i].CompareTag("Bat"))//PosCols[i] != prevPosCols[i])
+                    if (cols[i].CompareTag("GoalPost"))
+                    {
+                        Destroy(this.gameObject,1.0f);
+                        ballDir = Vector3.zero;
+                    }
+                    if (cols[i].CompareTag("Wall"))
+                    { //Pos != prevPos)
+                        ballDir = Vector3.Reflect(ballDir.normalized, Vector3.Normalize(cols[i].transform.position));
+                    }
+                    else if (cols[i].CompareTag("Bat"))//PosCols[i] != prevPosCols[i])
                         ballDir = transform.position - cols[i].transform.position;
                 }
 
             }
-            pos = transform.position;
         }
-        
-        prevPos = transform.position;
-
+        pos = transform.position;
+        transform.position = new Vector3(Mathf.Clamp(pos.x, -7.6f, 7.6f), 1, Mathf.Clamp(pos.z, -15.6f, 15.6f));
     }
 }
